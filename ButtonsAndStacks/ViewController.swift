62414268d1af5c1a0a9bbc47ed282620822021e7
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-09 19:39:53 +0200

    Section  2.12 - Mini App ButtonsAndStacks: Stacks, Multiple Buttons to 1 IBAction, Color Array

--- ButtonsAndStacks/ViewController.swift
+++ ButtonsAndStacks/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var colorLabel: UILabel!
 
+	
+	var colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .systemIndigo, .systemPurple ]
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
 	}
 
+	@IBAction func colorButtonPressed(_ sender: UIButton) {
+		print("You've clicked tag # \(sender.tag)")
+		if sender.tag == 1000 {
+			colorLabel.text = ""
+		} else {
+			colorLabel.text = "You clicked \(sender.currentTitle!)"
+			colorLabel.textColor = colors[sender.tag]
+		}
 
+	}
+	
+	
 }
 
